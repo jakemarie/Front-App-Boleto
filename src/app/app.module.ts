e644598d4b0f8f6c907204496fb050dbb9911e89
Jaqueline <jake.marie@hotmail.com>
2024-09-14 20:35:32 -0300

    Criado componente boleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoletoComponent } from './boleto/boleto.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    BoletoComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/boleto/boleto.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { BoletoComponent } from './boleto.component';
+
+describe('BoletoComponent', () => {
+  let component: BoletoComponent;
+  let fixture: ComponentFixture<BoletoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BoletoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoletoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
