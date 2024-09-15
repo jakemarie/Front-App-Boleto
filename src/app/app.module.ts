0abe4f540f300dbae2e39d85982765da72f6a9d7
Jaqueline <jake.marie@hotmail.com>
2024-09-14 23:16:43 -0300

    Criação do layout tela de boletos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoletoComponent } from './boleto/boleto.component';
+import { BoletoComponent } from './components/boleto/boleto.component';
 
 
 @NgModule({
