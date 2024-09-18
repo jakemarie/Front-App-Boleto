1b1a1ecce3732ec555ba66e22d3c02f499997daf
Jaqueline <jake.marie@hotmail.com>
2024-09-17 23:21:41 -0300

    Desenvolvido as chamadas da Api

--- src/app/components/boleto/boleto.component.ts
+++ src/app/components/boleto/boleto.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { BoletoService } from '../../service/boleto.service';
+import { Boleto } from '../../model/boleto';
 
 @Component({
   selector: 'app-boleto',
@@ -7,6 +9,9 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./boleto.component.css']
 })
 export class BoletoComponent implements OnInit {
+
+  dataAtual: any;
+  boletoList: Boleto[] = [];
 
   boletoForm = this.fb.group(
     {
@@ -17,9 +22,11 @@ export class BoletoComponent implements OnInit {
     }
   )
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private boletoService: BoletoService) { }
 
   ngOnInit() {
+    this.dataAtual = new Date().toISOString().slice(0, 10); 
+    this.buscarBoletoPorDataAtual();
   }
 
   salvarBoletos() {
@@ -32,6 +39,71 @@ export class BoletoComponent implements OnInit {
     }
     
   }
+
+  buscarBoletoPorDataAtual() {
+    this.boletoService.buscarBoletoPorDataAtual().subscribe({
+      next: (res) => {
+        this.boletoList = res.dados
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
+
+  buscarTodosBoleto() {
+    this.boletoService.buscarTodosBoletos().subscribe({
+      next: (res) => {
+        this.boletoList = res.dados
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
   
+  deletarBoleto(id: any) {
+    this.boletoService.deletarBoleto(id).subscribe({
+      next: (res) => {
+        this.boletoList = res.dados
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
+
+  salvarBoleto(boleto: Boleto) {
+    this.boletoService.salvarBoleto(boleto).subscribe({
+      next: (res) => {
+        this.boletoList = res.dados
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
+
+  editarBoleto(boleto: Boleto) {
+    this.boletoService.editararBoleto(boleto).subscribe({
+      next: (res) => {
+        this.boletoList = res.dados
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
+
+  buscarBoletoPorId(id: any) {
+    this.boletoService.buscarBoletoPorId(id).subscribe({
+      next: (res) => {
+        console.log(res.dados)
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+  }
 
 }
